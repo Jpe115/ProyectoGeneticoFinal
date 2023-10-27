861ba156ee9bd86d87bfd7d7912fc0939e5a7deb
Josue Palafox <jpalafoxe90@gmail.com>
2023-10-27 15:51:24 -0700

    Agregadas las variables necesarias

--- ProyectoGeneticoFinal/MainWindow.xaml.cs
+++ ProyectoGeneticoFinal/MainWindow.xaml.cs
@@ -14,14 +14,42 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Newtonsoft.Json;
+using System.IO;
 
 namespace ProyectoGeneticoFinal
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    enum TipoCruzamiento
+    {
+        TPX,
+        OPX
+    }
+
+    enum TipoMutación
+    {
+        Swap,
+        HSwap
+    }
+
     public partial class MainWindow : Window
     {
+        private Random rand = new Random();
+        private List<(int, int)> coordenadas = new List<(int, int)>();
+        private int cantidadPuntos = 15;
+        private int cantPoblación;
+        private int[,] distancias = new int[15, 15];      
+        private int[,] Población = new int[1, 1];
+        private int[,] Población2 = new int[1, 1];
+        private int probCruzamiento;
+        private int probMutación;
+        private bool esPob1Actual = true;
+        private int criterioParo = 100;
+        private TipoCruzamiento cruzamiento;
+        private TipoMutación mutación;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -31,6 +59,35 @@ namespace ProyectoGeneticoFinal
         private void btnEjecutar_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private async Task<bool> LeerPuntos()
+        {
+            string nombreArchivo = "D:\\Documentos\\Visual Studio\\ProyectoGenetico\\ProyectoGenetico\\bin\\Debug\\net7.0-windows10.0.17763.0\\CoordenadasGuardadas.json";
+            try
+            {
+                if (File.Exists(nombreArchivo))
+                {
+                    string json = await File.ReadAllTextAsync(nombreArchivo);
+                    List<(int, int)>? listaCoordenadas = JsonConvert.DeserializeObject<List<(int, int)>>(json);
+
+                    if (listaCoordenadas != null)
+                    {
+                        coordenadas = listaCoordenadas;
+                        return true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("No se ha encontrado una lista de ciudades", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
         }
     }
 }
